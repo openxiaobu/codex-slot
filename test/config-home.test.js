630a356b0f0653ef5480c425459b210513226869
buke <66482504@qq.com>
2026-07-25 22:19:58 +0800

    test(config): 修正 HOME 尾随空格断言

--- test/config-home.test.js
+++ test/config-home.test.js
@@ -7,12 +7,13 @@ const { getUserHomeDir } = require("../dist/config.js");
 test("getUserHomeDir 会裁剪 HOME 尾随空格", () => {
   const previousHome = process.env.HOME;
   const previousUserProfile = process.env.USERPROFILE;
+  const expectedHome = os.homedir();
 
   try {
-    process.env.HOME = `${os.homedir()} `;
+    process.env.HOME = `${expectedHome} `;
     delete process.env.USERPROFILE;
 
-    assert.equal(getUserHomeDir(), os.homedir());
+    assert.equal(getUserHomeDir(), expectedHome);
   } finally {
     if (previousHome === undefined) {
       delete process.env.HOME;
